802e7afa794b038283b7d014d335e15cfc80873d
Quddus <quddus_sap@outlook.com>
2022-12-08 11:12:41 +0530

    added product.json file and app.js with extra logic

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express')
 const path = require('path')
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname+"/index.html"))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/index.html"))
 })
 
-app.listen("3000",(req,res)=>{
-    console.log("Express is listing on 3000")
+app.get("/product/:id", (req, res) => {
+    
+})
+
+app.listen("3000", (req, res) => {
+    console.log("Express is listing on 30000")
 })
